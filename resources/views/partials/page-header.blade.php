ff94687761096b66c5cb7c2578f48f4868e5e098
Pierre Romera <hello@pirhoo.com>
2020-11-09 00:06:14 +0100

    Typo

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -6,7 +6,7 @@
       <div class="page__header__meta__author d-flex justify-content-start align-items-center">
         <img class="page__header__meta__author__avatar" src="@asset('images/ajl-full.png')" alt="logo AJL" />
         <p class="m-0 mx-3">
-          Par l'<a target="_blank" href="https://www.ajlgbt.info">Association des Journaliste LGBT</a>
+          Par l'<a target="_blank" href="https://www.ajlgbt.info">Association des Journalistes LGBT</a>
         </p>
         <div class="ml-auto text-capitalize">
           @php the_date('F Y') @endphp
