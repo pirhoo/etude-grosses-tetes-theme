0eb09de0e990c5ca036f752ac793af4f5b5655db
Pierre Romera <hello@pirhoo.com>
2020-11-08 19:31:43 +0100

    Started page integration

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -1,2 +1,14 @@
-@php the_content() @endphp
+@if (has_post_thumbnail())
+  <div class="page__thumbnail">
+    @php the_post_thumbnail('post-thumbnail', ['class' => 'page__thumbnail__image']) @endphp
+    <div class="page__thumbnail__caption">
+      @php the_post_thumbnail_caption() @endphp
+    </div>
+  </div>
+@endif
+
+<div class="page__content">
+  @php the_content() @endphp
+</div>
+
 {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,5 +1,18 @@
-<div class="page-header">
-  @if (!is_home())
-    <h1>{!! App::title() !!}</h1>
-  @endif
-</div>
+@if (!is_home())
+  <div class="page__header">
+      <h1 class="page__header__title">
+        {!! App::title() !!}
+      </h1>
+      <div class="page__header__meta">
+        <div class="page__header__meta__author d-flex justify-content-start align-items-center">
+          <img class="page__header__meta__author__avatar" src="@asset('images/ajl-full.png')" alt="logo AJL" />
+          <p class="m-0 mx-3">
+            Par l'<a target="_blank" href="https://www.ajlgbt.info">Association des Journaliste LGBT</a>
+          </p>
+          <div class="ml-auto text-capitalize">
+            @php the_date('F Y') @endphp
+          </div>
+        </div>
+      </div>
+  </div>
+@endif
