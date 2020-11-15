6738b9c7dc9246000e816670cdd5eba98e713d43
Pierre Romera <hello@pirhoo.com>
2020-11-15 16:42:18 +0100

    Updated wording

--- resources/views/partials/home-jumbotron.blade.php
+++ resources/views/partials/home-jumbotron.blade.php
@@ -3,10 +3,10 @@
     <div class="row align-items-center justify-content-center">
       <div class="col-7 home__jumbotron__punchline">
         <h1>
-          Une punchline pour présenter l'étude
+          Des remarques sexistes toutes les 11 minutes
         </h1>
         <p class="lead">
-          Et une courte description sympa pour les gens qui aiment lire des descriptions.
+          C'est ce que rêvèle note dernière étude sur Les Grosses Têtes. Avec en moyenne <strong class="font-weight-bold">19 épisodes problématiques par émission</strong>, blablabla
         </p>
       </div>
       <a class="col home__jumbotron__link mx-3"  href="{{ get_permalink(Chapter::first()) }}">
