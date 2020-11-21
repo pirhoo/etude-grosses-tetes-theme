d61bdde655b5b3b0ba69a063e1217dba477bd1eb
Pierre Romera <hello@pirhoo.com>
2020-11-21 12:02:10 +0100

    Redesign autio player in pages

--- resources/assets/scripts/routes/page.js
+++ resources/assets/scripts/routes/page.js
@@ -2,9 +2,14 @@ import GreenAudioPlayer from 'green-audio-player'
 
 export default {
   init() {
-    GreenAudioPlayer.init({
-      selector: '.wp-block-audio',
-      stopOthersOnPlay: true,
+    // Add a class to audo player with a legend
+    $('figcaption:not(:empty)', this.audioPlayerSelector).each((i, legend) => {
+      $(legend).closest(this.audioPlayerSelector).addClass('wp-block-audio--legend')
     })
+    // Create a "green audio player" on all wordpress audio tags
+    GreenAudioPlayer.init({ selector: this.audioPlayerSelector, stopOthersOnPlay: true })
+  },
+  get audioPlayerSelector () {
+    return '.wp-block-audio'
   },
 };
