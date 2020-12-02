ada4fee290e4cfdce3f2bfaa38779ab2d739c969
Pierre Romera <hello@pirhoo.com>
2020-12-02 13:48:22 +0100

    Added WaveSurfer to player

--- resources/assets/scripts/routes/page.js
+++ resources/assets/scripts/routes/page.js
@@ -1,4 +1,5 @@
 import GreenAudioPlayer from 'green-audio-player'
+import WaveSurfer from 'wavesurfer.js'
 
 export default {
   init() {
@@ -7,9 +8,41 @@ export default {
       $(legend).closest(this.audioPlayerSelector).addClass('wp-block-audio--legend')
     })
     // Create a "green audio player" on all wordpress audio tags
-    GreenAudioPlayer.init({ selector: this.audioPlayerSelector, stopOthersOnPlay: true })
+    this.initGreenAudioPlayer()
+    // Create a "wave" on all wordpress audio tags
+    this.initWaveSurfer()
+  },
+  initGreenAudioPlayer () {
+    GreenAudioPlayer.init({
+      selector: this.audioPlayerSelector,
+      stopOthersOnPlay: true,
+    })
+  },
+  initWaveSurfer () {
+    // On each player, we need to add a "wave" separetly
+    document.querySelectorAll(this.audioPlayerSelector).forEach(player => {
+      const container = player.querySelector('.controls__slider')
+      const audio = player.querySelector('audio')
+      // Create the wave element
+      const ws = WaveSurfer.create({
+        container,
+        height: 80,
+        waveColor: '#6c757d',
+        barWidth: 1,
+        progressColor: '#e20318',
+      })
+      // Load the song using the same src
+      ws.load(audio.src)
+      // Ensure the audio currentTime is reflected on
+      audio.addEventListener('timeupdate', () => {
+        ws.seekTo(audio.currentTime  / audio.duration)
+      })
+    })
   },
   get audioPlayerSelector () {
     return '.wp-block-audio'
+  },
+  get audioTagSelector () {
+    return '.wp-block-audio audio'
   },
 };
