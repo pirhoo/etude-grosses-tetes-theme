44cb8e335271431c3dcdc785eec36ccc70592835
Pierre Romera <hello@pirhoo.com>
2020-12-05 22:05:41 +0100

    More space

--- resources/views/partials/home-chapters.blade.php
+++ resources/views/partials/home-chapters.blade.php
@@ -1,12 +1,12 @@
 <div class="home__chapters">
   <div class="container home__chapters__panel">
     <div class="row no-gutters">
-      <div class="col-7">
-        <div class="py-4 pr-4 d-flex flex-column h-100">
+      <div class="col-6">
+        <div class="py-4 pr-5 d-flex flex-column h-100">
           @include('partials/home-interviews')
         </div>
       </div>
-      <div class="col home__chapters__panel__list">
+      <div class="col-6 home__chapters__panel__list">
         @foreach (FrontPage::studyChapters() as $chapter)
           <a href="{{ get_permalink($chapter) }}" class="home__chapters__panel__list__item d-flex text-dark">
             <div class="flex-grow-1">
