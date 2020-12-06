1dd35aed75f89e871540c84f38ec115bbc42aa03
Pierre Romera <hello@pirhoo.com>
2020-12-06 19:09:13 +0100

    Style search + typo on home

--- resources/views/partials/content-search.blade.php
+++ resources/views/partials/content-search.blade.php
@@ -1,2 +1,10 @@
-<article @php post_class() @endphp>
+<article @php post_class('list-group-item') @endphp>
+  <header>
+    <h2 class="entry-title">
+      <a href="{{ get_permalink() }}">{!! get_the_title() !!}</a>
+    </h2>
+  </header>
+  <div class="entry-summary">
+    @php the_excerpt() @endphp
+  </div>
 </article>
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,18 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
+  <div class="container py-5">
+    <h1 class="mb-5">
+      {!! App::title() !!}
+    </h1>
+
+    @if (!have_posts())
+      <div class="alert alert-warning">
+        {{ __('Sorry, no results were found.', 'sage') }}
+      </div>
+      {!! get_search_form(false) !!}
+    @endif
+
+    <div class="list-group">
+      @while(have_posts()) @php the_post() @endphp
+        @include('partials.content-search')
+      @endwhile
     </div>
-    {!! get_search_form(false) !!}
-  @endif
 
-  @while(have_posts()) @php the_post() @endphp
-    @include('partials.content-search')
-  @endwhile
-
-  {!! get_the_posts_navigation() !!}
+    {!! get_the_posts_navigation() !!}
+  </div>
 @endsection
