8c7486131f53560a47caf082c45acbd3dae9d4b8
Pierre Romera <hello@pirhoo.com>
2020-12-06 20:34:50 +0100

    Open external links in a new window

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,7 +1,14 @@
 export default {
   init() {
-    $('a[href="#"]').on('click', ev => {
-      ev.preventDefault();
-    });
+    // Disable default event for # links
+    $('a[href="#"]').on('click', ev => ev.preventDefault());
+    // Open external all links in a new window
+    $(this.externalLinksSelector).attr('target', '_blank');
+  },
+  get externalLinksSelector () {
+    return `a[href^=http]:not([href^="${this.host}"])`;
+  },
+  get host () {
+    return `${window.location.protocol}//${window.location.host}`;
   },
 };
