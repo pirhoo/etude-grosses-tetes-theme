3ee61102ab45d082510dfa4ecf1aeaffc331dcbc
Pierre Romera <hello@pirhoo.com>
2020-12-06 12:23:05 +0100

    Mobile layout for the footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
-  <div class="footer__container container d-flex">
+  <div class="footer__container container d-block d-md-flex text-center text-md-left">
     <p>
-      <img src="@asset('images/ajl-white.png')" height="18" /> Une étude de l'Association des Journalistes LGBTI
+      <img src="@asset('images/ajl-white.png')" height="18" class="mr-2" /> Une étude de l'Association des Journalistes LGBTI
     </p>
     <p class="ml-auto">
       © {{ App::year() }} - <a href="{{ get_permalink(get_page_by_title('Mentions Légales')) }}">Mentions Légales</a>
