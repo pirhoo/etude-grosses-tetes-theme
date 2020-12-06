d4e363142246b7454f9a5c5d092b6537b35ecd55
Pierre Romera <hello@pirhoo.com>
2020-12-06 12:49:55 +0100

    Added mobile layout for page header

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -3,11 +3,13 @@
       {!! App::title() !!}
     </h1>
     <div class="page__header__meta">
-      <div class="page__header__meta__author d-flex justify-content-start align-items-center">
-        <img class="page__header__meta__author__avatar" src="@asset('images/ajl-full.png')" alt="logo AJL" />
-        <p class="m-0 mx-3">
-          Par l'<a target="_blank" href="https://www.ajlgbt.info">Association des Journalistes LGBTI</a>
-        </p>
+      <div class="page__header__meta__author d-block d-sm-flex justify-content-start align-items-center">
+        <div class="d-flex flex-row-reverse flex-sm-row align-items-center">
+          <img class="page__header__meta__author__avatar" src="@asset('images/ajl-full.png')" alt="logo AJL" />
+          <p class="my-0 mr-auto ml-sm-2">
+            Par l'<a target="_blank" href="https://www.ajlgbt.info">Association des Journalistes LGBTI</a>
+          </p>
+        </div>
         <div class="ml-auto text-capitalize">
           @php the_date('F Y') @endphp
         </div>
